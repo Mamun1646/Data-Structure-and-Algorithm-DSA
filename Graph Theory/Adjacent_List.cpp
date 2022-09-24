8bc588ee049a6aa262b6dc4461c7f2fc3c13ae53
Al Mamun <mimbd01130004@gmail.com>
2022-09-24 22:04:08 +0600

    Graph Thoery

---
+++ Graph Theory/Adjacent_List.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int adj_mat[100][100];
+#define MAX 100000 //maximum node
+vector<int>adj_list[MAX];
+int main()
+{
+   freopen("input.txt", "r", stdin);
+
+    //vector<vector<int>> adj_list;
+    int node, edge;
+    cin >> node >> edge;
+     
+    for (int i = 0; i < edge; ++i)
+    {
+        int n1, n2;
+        cin >> n1 >> n2;
+        adj_list[n1].push_back(n2);
+        adj_list[n2].push_back(n1);
+
+    }
+
+    for (int i = 1; i <=node; ++i)
+    {     cout<< i << " : ";
+        for(auto u : adj_list[i])
+           cout<<u<<" ";
+           cout<<endl;
+    }
+
+
+
+}
---
+++ Graph Theory/Adjacent_Matrix.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int adj_mat[100][100];
+int main()
+{
+    freopen("input.txt", "r", stdin);
+
+    vector<vector<int>> adj_list;
+    int node, edge;
+    cin >> node >> edge;
+    for (int i = 0; i < edge; ++i)
+    {
+        int n1, n2;
+        cin >> n1 >> n2;
+        adj_mat[n1][n2] = 1;
+        adj_mat[n2][n1] = 1;
+    }
+
+    for (int i = 0; i < node; ++i)
+    {
+        for (int j = 0; j < edge; ++j)
+        {
+            cout << adj_mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    int x, y;
+    cin >> x >> y;
+
+    if (adj_mat[x][y])
+        cout << x << " is a neighbour of " << y << endl;
+    else
+        cout << x << " is a neighbour of " << y << endl;
+}
