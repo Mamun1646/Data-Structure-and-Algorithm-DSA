6b1bc040db76a57831140416b85e41c3221f348b
Mamun1646 <mimbd0113000gmail.com>
2022-10-23 13:38:17 +0600

    Ditect Cycle Undirected Graph using BFS

---
+++ 7 Days Challege Graph/Day 1/Detect A cycle in Undirected Graph using BFS.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+bool isCyclicBFS(int src, unordered_map<int, bool> &visited, unordered_map<int, vector<int>> &adj_list)
+{
+    unordered_map<int, int> parent;
+    parent[src] = -1;
+
+    visited[src] = true;
+    queue<int> q;
+    q.push(src);
+    while (!q.empty())
+    {
+        int front = q.front();
+        q.pop();
+        for (auto neighbour : adj_list[front])
+        {
+            if (neighbour != parent[front] && visited[neighbour])
+                return true;
+            else if (!visited[neighbour])
+            {
+                q.push(neighbour);
+                visited[neighbour] = true;
+                parent[neighbour] = front;
+            }
+        }
+    }
+    return false;
+}
+
+string cycleDetection(vector<vector<int>> &edges, int n, int m)
+{
+
+    // create Adjacency list
+    unordered_map<int, vector<int>> adj_list;
+    for (int i = 0; i < m; ++i)
+    {
+        int x = edges[i][0];
+        int y = edges[i][1];
+        adj_list[x].push_back(y);
+        adj_list[y].push_back(x);
+    }
+    // To handle  disconnected components
+    unordered_map<int, bool> visited;
+    for (int i = 0; i < n; ++i)
+    {
+        if (!visited[i])
+        {
+            bool ans = isCyclicBFS(i, visited, adj_list);
+            if (ans)
+                return "Yes";
+        }
+    }
+    return "No";
+}
