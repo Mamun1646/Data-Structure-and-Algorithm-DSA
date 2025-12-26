a039d9f05eb4ff11f6f4430717d7a8ed485d93d9
Md. Al Mamun Mim <121859829+MamunCrafts@users.noreply.github.com>
2025-12-27 01:37:51 +0600

    Add deleteByValue function to Linked List implementation

--- Linked List/add-node-at-the-end.cpp
+++ Linked List/add-node-at-the-end.cpp
@@ -36,6 +36,31 @@ struct Node *addNodeAtTheBeginning(Node *head, int value) {
   newNode->next = head;
   return newNode;
 }
+struct Node *deleteByValue(Node *head, int value) {
+  if (head == NULL)
+    cout << "Linkedlist is an empty!";
+
+  if (head->data == value) {
+    Node *temp = head;
+    head = head->next;
+    free(temp);
+    return head;
+  }
+  Node *current = head;
+  Node *prev = NULL;
+
+  while (current != NULL) {
+    if (current->data == value) {
+      prev->next = current->next;
+      free(current);
+      break;
+    }
+    prev = current;
+    current = current->next;
+  }
+
+  return head;
+}
 int main() {
   Node *head = (Node *)malloc(sizeof(struct Node));
   head->data = 10;
@@ -43,6 +68,7 @@ int main() {
   head = addNodeAtTheEnd(head, 20);
   head = addNodeAtTheEnd(head, 30);
   head = addNodeAtTheBeginning(head, 80);
+  head = deleteByValue(head, 30);
   traverse(head);
   return 0;
 }
